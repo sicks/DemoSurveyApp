97db52552eacd79fe4d991fe7fd21bd6d4ca3d59
sicks <tcopeland@dg3n.net>
2025-05-10 19:33:52 -0700

    add authentication helper, model and request spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it "has a valid factory" do
+    expect(user.valid?).to be true
+  end
+
+  describe "validates:" do
+    %w[email_address password].each do |attr|
+      specify "presence of #{attr}" do
+        user.send("#{attr}=".to_sym, nil)
+        expect(user.valid?).to be false
+      end
+    end
+  end
 end
--- spec/requests/surveys_spec.rb
+++ spec/requests/surveys_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Surveys", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:session) { create(:session, user:) }
+
+  before do
+    authenticate(session)
+  end
+
+  describe "GET /" do
+    before { get root_path }
+
+    it { expect(response).to have_http_status :ok }
   end
 end
