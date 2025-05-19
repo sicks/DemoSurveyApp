83814c0dcd88ad7c2284db578f62ab0b76481cf2
sicks <tcopeland@dg3n.net>
2025-05-19 00:09:01 -0700

    resize textareas to fit contents

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,8 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
+import autosize from "autosize"
 
 export default class extends Controller {
   connect() {
+    // handle submit succes or failure
     this.element.addEventListener("turbo:submit-end", this.handleSubmitEnd.bind(this))
+
+    // resize textarea on content chagne
+    this.element.querySelectorAll('textarea').forEach((el) => {
+      autosize(el)
+      el.addEventListener('input', this.handleTextAreaInput.bind(this))
+    })
   }
 
   submit() {
@@ -10,10 +18,16 @@ export default class extends Controller {
   }
 
   handleSubmitEnd(e) {
-    if (e.detail.success) {
-      this.element.closest(".card").classList.remove("invalid")
-    } else {
-      this.element.closest(".card").classList.add("invalid")
+    if (this.element.closest(".card")) {
+      if (e.detail.success) {
+        this.element.closest(".card").classList.remove("invalid")
+      } else {
+        this.element.closest(".card").classList.add("invalid")
+      }
     }
+  }
+
+  handleTextAreaInput(e) {
+    autosize(e.target)
   }
 }
